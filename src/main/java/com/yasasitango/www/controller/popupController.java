914fc6bb66db9252e1f6eb9361e299568b8f644c
DESKTOP-PVB5OV7\Mork <cjm2357@gmail.com>
2020-10-14 10:08:42 +0900

    popupcontroller 수정

--- src/main/java/com/yasasitango/www/controller/popupController.java
+++ src/main/java/com/yasasitango/www/controller/popupController.java
@@ -1,4 +1,4 @@
-package com.yasasitango.www;
+package com.yasasitango.www.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
